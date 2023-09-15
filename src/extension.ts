b17c53bad59d6e852358d84f28a1ed1b1564836d
KAHLYM <wrightci01w@gmail.com>
2023-09-15 22:17:48 +0100

    Add menu contributions for selectForCompare and compareWithSelected

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+var path = require('path');
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -9,6 +11,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "folder-comparator" is now active!');
+
+	let compareFromPath: string;
+	let compareToPath: string;
+
+	vscode.commands.executeCommand('setContext', 'folder-comparator.showCompareWithSelected', false);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -19,7 +26,22 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from Folder Comparator!');
 	});
 
+	let selectForCompare = vscode.commands.registerCommand('folder-comparator.selectForCompare', async (context: vscode.Uri) => {
+		compareFromPath = context.path;
+		console.log(`Selection made to compare from '${compareFromPath}' to '${compareToPath}'`)
+		vscode.window.showInformationMessage(`Selected '${path.basename(compareFromPath)}' for comparison`);
+		vscode.commands.executeCommand('setContext', 'folder-comparator.showCompareWithSelected', true);
+	});
+
+	let compareWithSelected = vscode.commands.registerCommand('folder-comparator.compareWithSelected', async (context: vscode.Uri) => {
+		compareToPath = context.path;
+		console.log(`Selection made to compare from '${compareFromPath}' to '${compareToPath}'`)
+		vscode.commands.executeCommand('setContext', 'folder-comparator.showCompareWithSelected', false);
+	});
+
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(selectForCompare);
+	context.subscriptions.push(compareWithSelected);
 }
 
 // This method is called when your extension is deactivated
