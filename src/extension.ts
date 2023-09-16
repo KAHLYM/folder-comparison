48813f7fb890551d2e77eec5c998d8be70db58de
KAHLYM <wrightci01s@gmail.com>
2023-09-16 11:27:38 +0100

    Integrate sample from https://github.com/microsoft/vscode-extension-samples/blob/main/tree-view-sample/src/fileExplorer.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as view from './view';
 
 var path = require('path');
 
@@ -40,6 +41,7 @@ export function activate(context: vscode.ExtensionContext) {
 		console.log(`Selection made to compare from '${compareFromPath}' to '${compareToPath}'`)
 		vscode.commands.executeCommand('setContext', 'folder-comparator.showCompareWithSelected', false);
 		vscode.commands.executeCommand('setContext', 'folder-comparator.showView', true);
+		new view.FileExplorer();
 	});
 
 	context.subscriptions.push(disposable);
---
+++ src/view.ts
@@ -0,0 +1,85 @@
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as utilities from './utilities';
+
+interface Entry {
+	uri: vscode.Uri;
+	type: vscode.FileType;
+}
+
+export class FileSystemProvider implements vscode.TreeDataProvider<Entry> {
+
+	constructor() { }
+
+	stat(uri: vscode.Uri): vscode.FileStat | Thenable<vscode.FileStat> {
+		return this._stat(uri.fsPath);
+	}
+
+	async _stat(path: string): Promise<vscode.FileStat> {
+		return new utilities.FileStat(await utilities.stat(path));
+	}
+
+	readDirectory(uri: vscode.Uri): [string, vscode.FileType][] | Thenable<[string, vscode.FileType][]> {
+		return this._readDirectory(uri);
+	}
+
+	async _readDirectory(uri: vscode.Uri): Promise<[string, vscode.FileType][]> {
+		const children = await utilities.readdir(uri.fsPath);
+
+		const result: [string, vscode.FileType][] = [];
+		for (let i = 0; i < children.length; i++) {
+			const child = children[i];
+			const stat = await this._stat(path.join(uri.fsPath, child));
+			result.push([child, stat.type]);
+		}
+
+		return Promise.resolve(result);
+	}
+
+
+	readFile(uri: vscode.Uri): Uint8Array | Thenable<Uint8Array> {
+		return utilities.readfile(uri.fsPath);
+	}
+
+	async getChildren(element?: Entry): Promise<Entry[]> {
+		if (element) {
+			const children = await this.readDirectory(element.uri);
+			return children.map(([name, type]) => ({ uri: vscode.Uri.file(path.join(element.uri.fsPath, name)), type }));
+		}
+
+		const workspaceFolder = (vscode.workspace.workspaceFolders ?? []).filter(folder => folder.uri.scheme === 'file')[0];
+		if (workspaceFolder) {
+			const children = await this.readDirectory(workspaceFolder.uri);
+			children.sort((a, b) => {
+				if (a[1] === b[1]) {
+					return a[0].localeCompare(b[0]);
+				}
+				return a[1] === vscode.FileType.Directory ? -1 : 1;
+			});
+			return children.map(([name, type]) => ({ uri: vscode.Uri.file(path.join(workspaceFolder.uri.fsPath, name)), type }));
+		}
+
+		return [];
+	}
+
+	getTreeItem(element: Entry): vscode.TreeItem {
+		const treeItem = new vscode.TreeItem(element.uri, element.type === vscode.FileType.Directory ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None);
+		if (element.type === vscode.FileType.File) {
+			treeItem.command = { command: 'fileExplorer.openFile', title: "Open File", arguments: [element.uri], };
+			treeItem.contextValue = 'file';
+		}
+		return treeItem;
+	}
+}
+
+export class FileExplorer {
+	constructor() {
+		const treeDataProvider = new FileSystemProvider();
+		vscode.window.createTreeView('folderComparator', { treeDataProvider });
+		vscode.commands.registerCommand('fileExplorer.openFile', (resource) => this.openResource(resource));
+	}
+
+	private openResource(resource: vscode.Uri): void {
+		vscode.window.showTextDocument(resource);
+	}
+}
