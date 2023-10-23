544b9e2cb563d7ba3a5e263388baf1ef4fb1c862
KAHLYM <wrightci01s@gmail.com>
2023-10-23 13:53:01 +0100

    Rename from comparator to comparison

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ var path = require('path');
 export class FileExplorer {
 	constructor(left: vscode.Uri, right: vscode.Uri) {
 		const treeDataProvider = new fsp.FileSystemProvider(left, right);
-		vscode.window.createTreeView('folderComparator', { treeDataProvider });
+		vscode.window.createTreeView('folderComparison', { treeDataProvider });
 		new esdp.EntryStateDecorationProvider(left, right);
 	}
 }
@@ -17,22 +17,22 @@ export function activate(context: vscode.ExtensionContext) {
 	let compareFromPath: vscode.Uri;
 	let compareToPath: vscode.Uri;
 
-	vscode.commands.executeCommand('setContext', 'folder-comparator.showCompareWithSelected', false);
-	vscode.commands.executeCommand('setContext', 'folder-comparator.showView', false);
+	vscode.commands.executeCommand('setContext', 'folder-comparison.showCompareWithSelected', false);
+	vscode.commands.executeCommand('setContext', 'folder-comparison.showView', false);
 
-	let selectForCompare = vscode.commands.registerCommand('folder-comparator.selectForCompare', async (uri: vscode.Uri) => {
+	let selectForCompare = vscode.commands.registerCommand('folder-comparison.selectForCompare', async (uri: vscode.Uri) => {
 		compareFromPath = uri;
 		console.log(`Selection made to compare from '${compareFromPath.path}'`)
 		vscode.window.showInformationMessage(`Selected '${path.basename(compareFromPath.path)}' for comparison`);
-		vscode.commands.executeCommand('setContext', 'folder-comparator.showCompareWithSelected', true);
-		vscode.commands.executeCommand('setContext', 'folder-comparator.showView', false);
+		vscode.commands.executeCommand('setContext', 'folder-comparison.showCompareWithSelected', true);
+		vscode.commands.executeCommand('setContext', 'folder-comparison.showView', false);
 	});
 
-	let compareWithSelected = vscode.commands.registerCommand('folder-comparator.compareWithSelected', async (uri: vscode.Uri) => {
+	let compareWithSelected = vscode.commands.registerCommand('folder-comparison.compareWithSelected', async (uri: vscode.Uri) => {
 		compareToPath = uri;
 		console.log(`Selection made to compare from '${compareFromPath.path}' to '${compareToPath.path}'`)
-		vscode.commands.executeCommand('setContext', 'folder-comparator.showCompareWithSelected', false);
-		vscode.commands.executeCommand('setContext', 'folder-comparator.showView', true);
+		vscode.commands.executeCommand('setContext', 'folder-comparison.showCompareWithSelected', false);
+		vscode.commands.executeCommand('setContext', 'folder-comparison.showView', true);
 
 		new FileExplorer(compareFromPath, compareToPath);
 	});
