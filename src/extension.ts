ed7ca529171e611cb75efe65f4395aa5af17dc8c
Callum Wright <wrightci01s@gmail.com>
2023-12-07 18:58:59 +0000

    Add telemetry using @vscode/extension-telemetry (#39)
    * Setup telemetry using @vscode/extension-telemetry
    
    * Add telemetry events
    
    * Configure telemetry key based on node environment
    
    * Do not send telemetry on non-user-driven activity
    
    * Add telemetry events
    
    * Add telemetry error events
    
    * Add subscription keys for production and development
    
    * Update subscription keys for production and development

--- src/extension.ts
+++ src/extension.ts
@@ -2,15 +2,26 @@ import * as vscode from 'vscode';
 import * as fsp from './file-system-provider';
 import * as esdp from './entry-state-decorator-provider';
 import { execSync } from 'child_process';
+import TelemetryReporter from '@vscode/extension-telemetry';
+import { isProduction } from './config'
 
 var path = require('path');
 
+let reporter: TelemetryReporter;
+const key = isProduction() ? '8fce904f-cf3a-4160-9107-82c024e9c258' : '8fce904f-cf3a-4160-9107-82c024e9c258';
+
 export function activate(context: vscode.ExtensionContext) {
+
+	reporter = new TelemetryReporter(key);
+	context.subscriptions.push(reporter);
+	reporter.sendTelemetryEvent('activation');
 
 	try {
 		execSync('git version');
 	} catch (err: any) {
 		if (!/\'git\' is not recognized as an internal or external command/.test(err.message || '')) {
+			// To protect user privacy, do not send error message in telemetry
+			reporter.sendTelemetryErrorEvent('git.version');
 			throw err;
 		}
 
@@ -30,6 +41,7 @@ export function activate(context: vscode.ExtensionContext) {
 	vscode.commands.executeCommand('setContext', 'folderComparison.showViewTitles', false);
 
 	let selectForCompare = vscode.commands.registerCommand('folderComparison.selectForCompare', async (uri: vscode.Uri) => {
+		reporter.sendTelemetryEvent('command.selectForCompare');
 		compareFromPath = uri;
 		console.info(`Selection made to compare from '${compareFromPath.path}'`)
 		vscode.window.showInformationMessage(`Selected '${path.basename(compareFromPath.path)}' for comparison`);
@@ -37,6 +49,7 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	let compareWithSelected = vscode.commands.registerCommand('folderComparison.compareWithSelected', async (uri: vscode.Uri) => {
+		reporter.sendTelemetryEvent('command.compareWithSelected');
 		compareToPath = uri;
 		console.info(`Selection made to compare from '${compareFromPath.path}' to '${compareToPath.path}'`)
 		vscode.commands.executeCommand('setContext', 'folderComparison.showCompareWithSelected', false);
@@ -46,15 +59,24 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	let openSettings = vscode.commands.registerCommand('folderComparison.openSettings', async () => {
-		vscode.commands.executeCommand( 'workbench.action.openSettings', '@ext:KAHLYM.folder-comparison');
+		reporter.sendTelemetryEvent('command.openSettings');
+		vscode.commands.executeCommand('workbench.action.openSettings', '@ext:KAHLYM.folder-comparison');
 	});
 
 	vscode.commands.registerCommand('folderComparison.clear', async () => {
+		reporter.sendTelemetryEvent('command.clear');
 		fileSystemProvider.clear();
 		vscode.commands.executeCommand('setContext', 'folderComparison.showViewTitles', false);
 	});
 
-	vscode.commands.registerCommand('folderComparison.refresh', async () => {
+	vscode.commands.registerCommand('folderComparison.refresh', async (telemetry: boolean = true) => {
+		// This `folderComparison.refresh` command is called from within `setInterval` and would raise
+		// telemetry too frequently. We do not need telemetry on non-user-driven activity so allow 
+		// function calls to opt-out of telemetry. If the user were to select the refresh button from
+		// the UI, `telemetry` would be `true`.
+		if (telemetry) {
+			reporter.sendTelemetryEvent('command.refresh');
+		}
 		fileSystemProvider.refresh();
 	});
 
@@ -64,20 +86,22 @@ export function activate(context: vscode.ExtensionContext) {
 
 	function setRefreshInterval(): NodeJS.Timer {
 		let intervalInSeconds = vscode.workspace.getConfiguration('folderComparison').get<number>('refreshInterval') ?? Number.MAX_SAFE_INTEGER;
-		return setInterval(fileSystemProvider.refresh.bind(fileSystemProvider), intervalInSeconds * 1000);
+		// @ts-ignore
+		return setInterval(fileSystemProvider.refresh.bind(fileSystemProvider, false), intervalInSeconds * 1000);
 	}
 
 	let refreshInterval = setRefreshInterval();
-	
+
 	vscode.workspace.onDidChangeConfiguration(event => {
-        if (event.affectsConfiguration("folderComparison.refreshInterval")) {
+		if (event.affectsConfiguration("folderComparison.refreshInterval")) {
 			clearInterval(refreshInterval);
 			refreshInterval = setRefreshInterval();
-        }
-    })
+		}
+	})
 }
 
 async function warnAboutMissingGit(): Promise<void> {
+	reporter.sendTelemetryEvent('git.warning');
 	const download = 'Download Git';
 	const choice = await vscode.window.showWarningMessage(
 		'Git not found. Please install Git.',
@@ -85,7 +109,10 @@ async function warnAboutMissingGit(): Promise<void> {
 	);
 
 	if (choice === download) {
+		reporter.sendTelemetryEvent('git.warning.download');
 		vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('https://aka.ms/vscode-download-git'));
+	} else {
+		reporter.sendTelemetryEvent('git.warning.dismiss');
 	}
 }
 
