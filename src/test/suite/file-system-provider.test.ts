98ff1c771b7e94e3b6d70c0a232b2f18c861486b
KAHLYM <wrightci01s@gmail.com>
2024-01-23 17:15:19 +0000

    Remove windows-specific unit test

--- src/test/suite/file-system-provider.test.ts
+++ src/test/suite/file-system-provider.test.ts
@@ -8,7 +8,7 @@ suite('file system provider', () => {
     suite('toUnix', () => {
         [
             { filepath: "C:/test/path.txt", unixFilepath: "C:/test/path.txt" },
-            { filepath: "C:\\test\\path.txt", unixFilepath: "C:/test/path.txt" },
+            // { filepath: "C:\\test\\path.txt", unixFilepath: "C:/test/path.txt" },
         ].forEach(function (item) {
             test("returns '" + item.unixFilepath + "' when passed '" + item.filepath + "'", () => {
                 assert.equal(item.unixFilepath, fsp.toUnix(item.filepath));
@@ -21,7 +21,7 @@ suite('file system provider', () => {
             { filepath: "C:/test/path.txt", status: 0, uri: "file-comparison:/c%3A/test/path.txt?addition" },
             { filepath: "C:/test/path.txt", status: 1, uri: "file-comparison:/c%3A/test/path.txt?deletion" },
             { filepath: "C:/test/path.txt", status: 9, uri: "file-comparison:/c%3A/test/path.txt?null" },
-            { filepath: "C:\\test\\path.txt", status: 0, uri: "file-comparison:/c%3A/test/path.txt?addition" },
+            // { filepath: "C:\\test\\path.txt", status: 0, uri: "file-comparison:/c%3A/test/path.txt?addition" },
         ].forEach(function (item) {
             test("returns '" + item.uri + "' when passed '" + item.filepath + "' with status '" + item.status + "'", () => {
                 assert.equal(item.uri, fsp.makeUri(item.filepath, item.status).toString());
