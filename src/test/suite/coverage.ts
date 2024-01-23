31d9ab593902ec645a3e0cf1dedcb3d7fea8235c
KAHLYM <wrightci01s@gmail.com>
2024-01-23 17:08:10 +0000

    Improve file-system-provider.ts coverage

--- src/test/suite/coverage.ts
+++ src/test/suite/coverage.ts
@@ -86,8 +86,7 @@ export async function run(): Promise<void> {
 
 	const skipFiles = [
 		'config.ts',
-		'extension.ts',
-		'file-system-provider.ts'];
+		'extension.ts'];
 	filenames = filenames.concat(skipFiles);
 
 	const gitDirectory = path.join(__dirname, '..', '..', '..', 'src');
