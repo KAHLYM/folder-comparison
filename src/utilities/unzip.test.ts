476a19b2cdc3c4775ca65054357c7ed328a4f8c8
KAHLYM <wrightci01s@gmail.com>
2024-02-12 19:22:27 +0000

    Fix _isDirectory test

--- src/utilities/unzip.test.ts
+++ src/utilities/unzip.test.ts
@@ -3,14 +3,23 @@ import { _isDirectory } from './unzip';
 
 suite('unzip', () => {
 
+    class MockEntry {
+        constructor(filename: string) {
+            this.fileName = filename;
+        }
+
+        public fileName: string;
+    }
+
     suite('_isDirectory', () => {
         [
             { path: "C:/test/path.txt", isDirectory: false },
-            { path: "C:/test.txt/path", isDirectory: true },
+            { path: "C:/test.txt/path", isDirectory: false },
             { path: "C:/test/path/", isDirectory: true },
         ].forEach(function (item) {
             test(`return expected given ${item.path}`, () => {
-                assert.equal(item.isDirectory, _isDirectory(item.path));
+                const entry = new MockEntry(item.path);
+                assert.equal(item.isDirectory, _isDirectory(entry));
             });
         });
     });
